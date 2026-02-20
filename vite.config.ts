455ce6139edf9db5e2296fa1d805c9f658560992
Priyanka <priyankaspm04@gmail.com>
2026-02-21 00:32:21 +0530

    deploy try

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/docs-lth-on-fsl/",
   server: {
     host: "::",
     port: 8080,
